69fabed672a3ea326b09abf92c716298cca97c1a
MarcosCN1105 <marcoscn54@educastur.es>
2025-11-19 14:05:36 +0100

    19/11/2025 avances

---
+++ clases/tipoCombustible.enum.php
@@ -0,0 +1,9 @@
+<?php
+enum TipoGasolina {
+    case Gasolina;
+    case Diesel;
+    case Electrico;
+    case Hibrido;
+    case Gas;
+}
+?>
--- conexiones/cochesDao.class.php
+++ conexiones/cochesDao.class.php
@@ -1,13 +1,16 @@
-<?php 
+<?php
+
 require_once("marca.class.php");
 require_once("modelo.class.php");
 
-class cochesDao{
-    public function getMarcas(){
+class cochesDao
+{
+    public function getMarcas()
+    {
         $conexion = Conexion::getInstancia()->getConexion();
         $resultado = $conexion->query("SELECT * FROM marcas");
         $marcas = [];
-        while($fila=$resultado->fetch(PDO::FETCH_OBJ)){
+        while ($fila = $resultado->fetch(PDO::FETCH_OBJ)) {
             $marca = new Marca(
                 $fila->id,
                 $fila->nombre
@@ -17,12 +20,13 @@ class cochesDao{
         return $marcas;
     }
 
-    public function getModelos($marcaId){
+    public function getModelos($marcaId)
+    {
         $conexion = Conexion::getInstancia()->getConexion();
         $resultado = $conexion->prepare("SELECT * FROM modelos WHERE marca_id = ?");
         $resultado->execute([$marcaId]);
         $modelos = [];
-        while($fila=$resultado->fetch(PDO::FETCH_OBJ)){
+        while ($fila = $resultado->fetch(PDO::FETCH_OBJ)) {
             $modelo = new Modelo(
                 $fila->id,
                 $fila->marca_id,
@@ -33,5 +37,71 @@ class cochesDao{
         }
         return $modelos;
     }
+
+    public function getCombustibles()
+    {
+        $conexion = Conexion::getInstancia()->getConexion();
+        $resultado = $conexion->query("SELECT * FROM combustibles");
+        $combustibles = [];
+
+        while ($fila = $resultado->fetch(PDO::FETCH_OBJ)) {
+            $combustible = new Combustible(
+                $fila->id,
+                $fila->autonomia,
+                TipoGasolina::from($fila->tipo)
+            );
+            $combustibles[] = $combustible;
+        }
+
+        return $combustibles;
+    }
+
+    public function getCombustiblesPorModelo(int $modeloId)
+    {
+        $conexion = Conexion::getInstancia()->getConexion();
+
+        $consulta = "
+        SELECT c.*
+        FROM combustibles c
+        INNER JOIN modelo_combustible mc ON c.id = mc.combustible_id
+        WHERE mc.modelo_id = ?
+    ";
+
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute([$modeloId]);
+
+        $combustibles = [];
+
+        while ($fila = $resultado->fetch(PDO::FETCH_OBJ)) {
+            $combustible = new Combustible(
+                $fila->id,
+                $fila->autonomia,
+                TipoGasolina::from($fila->tipo)
+            );
+            $combustibles[] = $combustible;
+        }
+
+        return $combustibles;
+    }
+
+
+    public function getTipoVehiculoPorModelo(int $modeloId): ?TipoVehiculo {
+    $conexion = Conexion::getInstancia()->getConexion();
+    $consulta = $conexion->prepare(
+        "SELECT t.tipo_vehiculo 
+         FROM tipovehiculo t
+         INNER JOIN modelos m ON t.id = m.tipovehiculo_id
+         WHERE m.id = ?"
+    );
+    $consulta->execute([$modeloId]);
+    $fila = $consulta->fetch(PDO::FETCH_OBJ);
+
+    if ($fila) {
+        return TipoVehiculo::from($fila->tipo_vehiculo);
+    }
+
+    return null; // Si no existe el modelo
 }
-?>
+
+
+}
