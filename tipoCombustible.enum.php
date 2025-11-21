31c5b4bf4d820005ab90881ea0f1edbf2e0f2449
roibf2000 <wyy05336@educastur.es>
2025-11-21 11:59:08 +0100

    a

--- tipoCombustible.enum.php
+++
@@ -1,9 +0,0 @@
-<?php
-enum tipoCombustible: string {
-    case Gasolina = 'Gasolina';
-    case Diesel = 'Diesel';
-    case Electrico = 'Electrico';
-    case Hibrido = 'Hibrido';
-    case Gas = 'Gas';
-}
-?>
